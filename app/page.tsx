bc34ee7690d584099671be6b5e44b274bb62a393
Ricardo Dias <ricardodias.dev@gmail.com>
2023-07-09 19:48:17 -0300

    Implementando CarCard em page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
-import { Hero, SearchBar, CustomFilter } from "@/components";
+import { Hero, SearchBar, CustomFilter, CarCard } from "@/components";
 import { fetchCars } from "@/utils";
 
 export default async function Home() {
   const allCars = await fetchCars();
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 0 || !allCars;
 
   return (
     <main className="overflow-hidden">
@@ -32,6 +34,20 @@ export default async function Home() {
             <CustomFilter title="fuel" />
             <CustomFilter title="year" />
           </div>
+          {!isDataEmpty ? (
+            <section>
+              <div className="home__cars-wrapper">
+                {allCars?.map((car) => (
+                  <CarCard car={car} />
+                ))}
+              </div>
+            </section>
+          ) : (
+            <div className="home__error-container">
+              <h2 className="text-black text-xl font-bold">Oops, no results...</h2>
+              <p>{allCars?.message}</p>
+            </div>
+          )}
         </div>
       </div>
     </main>
