48fbc5191d0c63aaf556c5d4872622d40b3ae21b
Ricardo Dias <ricardodias.dev@gmail.com>
2023-07-12 13:46:13 -0300

    Desenvolvendo page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
 import { Hero, SearchBar, CustomFilter, CarCard } from "@/components";
+import ShowMore from "@/components/ShowMore";
+import { fuels, yearsOfProduction } from "@/constants";
 import { fetchCars } from "@/utils";
 
-export default async function Home() {
-  const allCars = await fetchCars();
+export default async function Home({ searchParams }) {
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || "",
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || "",
+    limit: searchParams.limit || 10,
+    model: searchParams.model || "",
+  });
 
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 0 || !allCars;
 
@@ -31,8 +39,8 @@ export default async function Home() {
         <div className="home__filters">
           <SearchBar />
           <div className="home__filter-container">
-            <CustomFilter title="fuel" />
-            <CustomFilter title="year" />
+            <CustomFilter title="fuel" options={fuels} />
+            <CustomFilter title="year" options={yearsOfProduction} />
           </div>
           {!isDataEmpty ? (
             <section>
@@ -41,10 +49,16 @@ export default async function Home() {
                   <CarCard car={car} />
                 ))}
               </div>
+              <ShowMore
+                pageNumber={(searchParams.pageNumber || 10) / 10}
+                isNext={(searchParams.limit || 10) > allCars.length}
+              />
             </section>
           ) : (
             <div className="home__error-container">
-              <h2 className="text-black text-xl font-bold">Oops, no results...</h2>
+              <h2 className="text-black text-xl font-bold">
+                Oops, no results...
+              </h2>
               <p>{allCars?.message}</p>
             </div>
           )}
